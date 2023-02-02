31c975444744e6d74c70da6216f5c183a6fff82a
noel bothwell <20094424@mail.wit.ie>
2023-02-02 14:36:18 +0000

    added an enter button to open up to a new activity

--- app/src/main/java/ie/wit/assignment1/MainActivity.kt
+++ app/src/main/java/ie/wit/assignment1/MainActivity.kt
@@ -1,11 +1,27 @@
 package ie.wit.assignment1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById (R.id.EnterButton) as Button
+        button.setOnClickListener() {
+            openArchive()
+
+
+        }
     }
+
+    private fun openArchive(){
+    val intent = Intent(this, Archive::class.java)
+            startActivity(intent)
+
+}
 }
